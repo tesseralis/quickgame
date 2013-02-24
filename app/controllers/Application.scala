78e2067deb1ae310dc6be21c36798bc3043ecd0a
Nathan Alison <nathan.f.alison@gmail.com>
2013-02-23 23:55:29 -0600

    Add the routes for the basic API.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,9 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+
+  def gameIndex(gameName: String) = TODO
+
+  def game(gameName: String, id: String) = TODO
   
 }
