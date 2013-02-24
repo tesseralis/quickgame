280a2b0c748843e3c2b56166963987a744fab2c0
Nathan Alison <nathan.f.alison@gmail.com>
2013-02-24 13:23:27 -0600

    Add a crude tictactoe client thing.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.concurrent._
 
 object Application extends Controller {
   
@@ -9,8 +11,24 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def gameIndex(gameName: String) = TODO
+  def gameIndex(gameName: String) = Action {
+    if (gameName == "tictactoe") {
+      Ok("Welcome to our tic-tac-toe page!")
+    } else {
+      NotFound("Sorry, this game hasn't been implemented yet.")
+    }
+  }
 
-  def game(gameName: String, id: String) = TODO
-  
+  def game(gameName: String, id: String) = Action {
+    if (gameName == "tictactoe") {
+      Ok(views.html.tictactoe(id))
+    } else {
+      NotFound("Page not found")
+    }
+  }
+
+  def stream(gameName: String, id: String) = TODO
+//  def stream(gameName: String, id: String) = WebSocket.async[JsValue] { request =>
+//    Promise.pure(0)
+//  }
 }
