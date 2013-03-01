47499ce6cdecf6d8147d18c8a321f31d7a6a658c
Nathan Alison <nathan.f.alison@gmail.com>
2013-03-01 16:18:30 -0600

    Add the receive Join function of the new room.

--- app/models/GameRoom.scala
+++ app/models/GameRoom.scala
@@ -1,11 +1,15 @@
 package models
 
 import scala.util.{Try, Success, Failure}
+import scala.concurrent.duration._
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, Props}
+import akka.pattern.{ask, pipe}
+import akka.util.Timeout
 
 import play.api.libs.json._
 import play.api.libs.iteratee.{Iteratee, Concurrent}
+import play.api.libs.concurrent.Execution.Implicits._
 
 import utils.generateId
 
@@ -67,6 +71,99 @@ class Client(messageFromJson: JsValue => Option[ServerMessage]) extends Actor {
 
     case msg: ClientMessage[_] =>
       channel.push(msg.toJson)
+  }
+}
+
+trait Room[State, Mov] extends Actor {
+  import RoomState._
+
+  implicit val timeout = Timeout(10.seconds)
+
+  /*
+   * The following defines the public interface for this class.
+   * Override these methods to implement room behavior.
+   */
+
+  /** The number of players needed to play the game. */
+  def maxPlayers: Int
+
+  /** How to convert a move from JSON input. */
+  def moveFromJson(input: JsValue): Option[Mov]
+
+  /** How to transform a state into JSON. */
+  def stateToJson(input: State): JsValue
+
+  /** Move from one action to another. */
+  def move(state: State, idx: Int, mv: Mov): Try[State]
+
+  /** Initial state of the game. */
+  def initState: State
+
+  /** Marker on the end of the game. */
+  def gameEnd(state: State): Boolean
+
+
+  def dataFromJson(kind: String, data: JsValue): Option[ServerMessage] = {
+    None // todo implement...
+  }
+
+  /* Private state variables (to be replaced with an FSM */
+  /** A list of names of members. */
+  private[this] var members = Map[ActorRef, String]()
+  /** A list of current players. */
+  private[this] var players = Map[ActorRef, Int]()
+  /** The state of the game. */
+  private[this] var gameState: State = initState
+  /** The state of the room. */
+  private[this] var roomState: RoomState = Lobby
+
+  /** Utility functions that transform our stored data. */
+  def playersByIndex: Map[Int, ActorRef] = players map { _.swap }
+  def memberNames = members.values.toSeq
+  def playerNames = (0 until maxPlayers).map {i => 
+    playersByIndex.get(i).map(members).getOrElse("")
+  }
+
+  /* Additional messages specific to states. */
+  case class Move(move: Mov) extends ServerMessage
+  case class GameState(data: State) extends ClientMessage[State] {
+    override def dataToJson = stateToJson(gameState)
+  }
+
+  /** Send a message to all members */
+  def notifyAll[A](msg: ClientMessage[A]) {
+    for (child <- context.children) {
+      child ! msg
+    }
+  }
+
+  override def receive = {
+    case msg @ Join(name) => {
+      // Constructor?
+      val client = context.actorOf(Props[Client])
+
+      // Send the websocket to the sender
+      (client ? msg) pipeTo sender
+
+      // Assign the name to the member
+      members += (client -> name.getOrElse(generateId(10)))
+      notifyAll(Members(memberNames))
+
+      // Make a player if spots are available
+      if (players.size < maxPlayers) {
+        players += (client -> (0 until maxPlayers).indexWhere(!playersByIndex.contains(_)))
+        notifyAll(Players(playerNames))
+      }
+
+      // If we have the required number of players, start or resume the game
+      if (players.size == maxPlayers && roomState != Playing) {
+        if (roomState == Lobby) {
+          gameState = initState
+          notifyAll(GameState(gameState))
+        }
+        roomState = Playing
+      }
+    }
   }
 }
 
