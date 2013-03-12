6911e30b9fa3973a845f7d998d8f27848ef87883
Nathan Alison <nathan.f.alison@gmail.com>
2013-03-12 14:13:29 -0500

    Add the abstract Game class.

---
+++ app/models/Game.scala
@@ -0,0 +1,24 @@
+import scala.util.Try
+
+/**
+ * The base trait that defines the necessary elements of a game.
+ */
+trait Game {
+  /** Players are represented as integers. */
+  type Player = Int
+
+  /** The abstract type of the internal game state. */
+  type State
+
+  /** The abstract type of an action in the game. */
+  type Action
+  
+  /** The initial state of the game. */
+  def init: State
+
+  /** Returns true if the game has reached a stopping point. */
+  def isFinal(q: State): Boolean
+
+  /** The result of a player moving somewhere. */
+  def transition(q: State, a: Action, p: Player): Try[State]
+}
