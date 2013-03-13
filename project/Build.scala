539dbb26dad3ae050ebcdb2e19ca3d7cb5f7b494
Nathan Alison <nathan.f.alison@gmail.com>
2013-03-12 23:52:23 -0500

    Switch to ScalaTest for testing.

--- project/Build.scala
+++ project/Build.scala
@@ -12,13 +12,15 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "org.scala-lang" % "scala-compiler" % "2.10.0"
+      "org.scala-lang" % "scala-compiler" % "2.10.0",
+      "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
       routesImport ++= Seq("binders._", "_root_.utils._"),
       templatesImport ++= Seq("_root_.utils._"),
+      testOptions in Test := Nil, // No imports for ScalaTest
       Play2WarKeys.servletVersion := "3.0"
     ).settings(Play2WarPlugin.play2WarSettings: _*)
 
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,6 +1,8 @@
 package test
 
-import org.specs2.mutable._
+import org.scalatest._
+import org.scalatest.OptionValues._
+import org.scalatest.matchers.ShouldMatchers
 
 import play.api.test._
 import play.api.test.Helpers._
@@ -10,15 +12,12 @@ import play.api.test.Helpers._
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends FlatSpec with ShouldMatchers {
   
-  "Application" should {
-    "render the index page" in new WithApplication {
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("quickga.me")
-    }
+  "Application" should "render the index page" in new WithApplication {
+    val home = route(FakeRequest(GET, "/")).get
+    status(home) should be (OK)
+    contentType(home).value should be ("text/html")
+    contentAsString(home) should include ("quickga.me")
   }
 }
