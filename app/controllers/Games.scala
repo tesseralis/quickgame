74a59daa035ee3a85c4351f445a98e49575df11d
Nathan Alison <nathan.f.alison@gmail.com>
2013-03-15 18:39:12 -0500

    Remove the Chat `game`.
    Finally got rid of the stupid bugger.

--- app/controllers/Games.scala
+++ app/controllers/Games.scala
@@ -10,17 +10,13 @@ object Games {
    * Enumeration of the available games we have.
    * Edit this to define the games available in the application.
    */
-  def values: Set[GameType] = Set(Chat, TicTacToe, ConnectFour)
+  def values: Set[GameType] = Set(TicTacToe, ConnectFour)
 
   /** Mapping from the a uri-string to the game name. */
   def fromString: Map[String, GameType] = values.map(g => g.toString.toLowerCase -> g).toMap
 
   // DEFINE GAME CONTROLS HERE
   // todo We should really make each game a sub-project and define the types there. 
-  case object Chat extends GameType {
-    override def view = views.html.chat.render
-    override def props = Props[ChatRoom]
-  }
   case object TicTacToe extends GameType {
     override def view = views.html.tictactoe.render
     override def props = Props[TicTacToeRoom]
