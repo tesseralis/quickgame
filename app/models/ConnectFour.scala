74878c47f9781cf428323214b76cd358da1e0a83
Nathan Alison <nathan.f.alison@gmail.com>
2013-03-15 21:39:05 -0500

    Rearrange methods in ConnectFour.

--- app/models/ConnectFour.scala
+++ app/models/ConnectFour.scala
@@ -7,38 +7,14 @@ import play.api.libs.iteratee.{Iteratee, Concurrent}
 import play.api.libs.json.{Json, JsValue}
 
 object ConnectFour extends Game with GameFormat {
+
   type Board = IndexedSeq[Seq[Player]]
+
   override type Move = Int
 
-  def nextPlayer(p: Player): Player = 1 - p
+  override def numPlayers = 2
 
-  def winningMove(board: Board, col: Move, player: Player): Boolean = {
-    val defaultBoard = (for {
-      j <- 0 until 7
-      i <- 0 until board(j).length
-    } yield {
-      (i, j) -> board(j)(i)
-    }).toMap.withDefaultValue(-1)
-    val row = board(col).length - 1
-
-    val colWin = board(col).reverse.takeWhile(_ == player).length >= 4
-    val rowWin = {
-      val left = (1 to 3).takeWhile(k => defaultBoard((row, col-k)) == player).length
-      val right = (1 to 3).takeWhile(k => defaultBoard((row, col+k)) == player).length
-      (1 + left + right) >= 4
-    }
-    val diagWin = {
-      val left = (1 to 3).takeWhile(k => defaultBoard((row-k, col-k)) == player).length
-      val right = (1 to 3).takeWhile(k => defaultBoard((row+k, col+k)) == player).length
-      (1 + left + right) >= 4
-    }
-    val diagWin2 = {
-      val left = (1 to 3).takeWhile(k => defaultBoard((row-k, col+k)) == player).length
-      val right = (1 to 3).takeWhile(k => defaultBoard((row+k, col-k)) == player).length
-      (1 + left + right) >= 4
-    }
-    colWin || rowWin || diagWin || diagWin2
-  }
+  override def init = GameStart((0 until 7) map { _ => List.empty }, 0)
 
   trait State extends AbstractState {
     def board: Board
@@ -69,7 +45,6 @@ object ConnectFour extends Game with GameFormat {
   /** Contains the state of the board at end game and the winning player (-1 if none) */
   case class GameEnd(board: Board, player: Player) extends State
 
-  override def numPlayers = 2
   override def moveFromJson(data: JsValue) = data.asOpt[Int]
   override def stateToJson(state: State) = {
     val (stateString, player) = state match {
@@ -82,5 +57,34 @@ object ConnectFour extends Game with GameFormat {
       "board" -> Json.toJson(state.board)
     )
   }
-  override def init = GameStart((0 until 7) map { _ => List.empty }, 0)
+
+  def nextPlayer(p: Player): Player = 1 - p
+
+  def winningMove(board: Board, col: Move, player: Player): Boolean = {
+    val defaultBoard = (for {
+      j <- 0 until 7
+      i <- 0 until board(j).length
+    } yield {
+      (i, j) -> board(j)(i)
+    }).toMap.withDefaultValue(-1)
+    val row = board(col).length - 1
+
+    val colWin = board(col).reverse.takeWhile(_ == player).length >= 4
+    val rowWin = {
+      val left = (1 to 3).takeWhile(k => defaultBoard((row, col-k)) == player).length
+      val right = (1 to 3).takeWhile(k => defaultBoard((row, col+k)) == player).length
+      (1 + left + right) >= 4
+    }
+    val diagWin = {
+      val left = (1 to 3).takeWhile(k => defaultBoard((row-k, col-k)) == player).length
+      val right = (1 to 3).takeWhile(k => defaultBoard((row+k, col+k)) == player).length
+      (1 + left + right) >= 4
+    }
+    val diagWin2 = {
+      val left = (1 to 3).takeWhile(k => defaultBoard((row-k, col+k)) == player).length
+      val right = (1 to 3).takeWhile(k => defaultBoard((row+k, col-k)) == player).length
+      (1 + left + right) >= 4
+    }
+    colWin || rowWin || diagWin || diagWin2
+  }
 }
