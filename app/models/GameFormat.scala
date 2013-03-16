22d4630976ef0ea671aea91e69b67aad1b3ba580
Nathan Alison <nathan.f.alison@gmail.com>
2013-03-15 21:06:12 -0500

    Add a TODO to remember to fix JSON writes/reads.

--- app/models/GameFormat.scala
+++ app/models/GameFormat.scala
@@ -3,6 +3,7 @@ package models
 import play.api.libs.json.JsValue
 
 trait GameFormat { this: Game =>
+  // TODO: Use Reads and Writes instead.
   /** How to convert a move from JSON input. */
   def moveFromJson(input: JsValue): Option[Move]
 
