268f27a72401f6f6ac87c3f4c43094fb0668873a
Nathan Alison <nathan.f.alison@gmail.com>
2013-03-28 20:27:45 -0500

    Add a change-name test.
    Doesn't work, must have to do with asynchronicity...

--- app/models/Room.scala
+++ app/models/Room.scala
@@ -7,13 +7,18 @@ import common.models.Game1
 object Room {
   /** Message sent by a user wanting to join. */
   case class Join(name: String)
+  
+  // Room messages
+  case class ChangeName(name: String)
+
+  case class MemberData(name: String)
 
   object State extends Enumeration { 
     val Idle, Playing = Value 
   }
   type State = State.Value
 
-  case class Data[GS](members: Set[ActorRef], gamestate: GS)
+  case class Data[GS](members: Map[ActorRef, MemberData], gamestate: GS)
 }
 
 import Room._
@@ -24,15 +29,18 @@ import Room.State._
  * @param game The game definition used for this room
  */
 class Room[GS](game: Game1[GS, _]) extends Actor with FSM[State, Data[GS]] {
-  startWith(Idle, Data(Set.empty, game.init))
+  startWith(Idle, Data(Map.empty, game.init))
 
   when(Idle) {
     case Event(Join(name), Data(members, gamestate)) => 
       context.watch(sender)
-      stay using Data(members + sender, gamestate)
+      stay using Data(members + (sender -> MemberData(name)), gamestate)
 
     case Event(Terminated(client), Data(members, gamestate)) =>
       stay using Data(members - client, gamestate)
+
+    case Event(ChangeName(name), Data(members, gamestate)) =>
+      stay using Data(members.updated(sender, MemberData(name)), gamestate)
   }
 
   initialize
--- test/RoomSpec.scala
+++ test/RoomSpec.scala
@@ -1,6 +1,6 @@
 package test
 
-import org.scalatest.{FlatSpec, OneInstancePerTest}
+import org.scalatest._
 
 import akka.actor.{Actor, ActorRef, ActorSystem}
 import akka.testkit.{TestKit, TestActorRef, TestFSMRef, ImplicitSender}
@@ -28,36 +28,87 @@ class MockClient(val room: ActorRef) extends Actor {
 }
 
 class RoomSpec(_system: ActorSystem) extends TestKit(_system) 
-    with FlatSpec
+    with WordSpec
+    with BeforeAndAfterAll
     with OneInstancePerTest {
 
   def this() = this(ActorSystem("RoomSpec"))
 
   val room = TestFSMRef(new Room(MockGame))
 
-  behavior of "A game room"
-
-  it should "start properly" in {
-    assert(room.stateData.members.isEmpty)
-    info("Starts with no members.")
-
-    assert(room.stateData.gamestate === MockGame.init)
-    info("Starts with the initial game state.")
-
-    assert(room.stateName === State.Idle)
-    info("Starts in the Idle state.")
+  override def afterAll {
+    system.shutdown()
   }
 
-  it should "add and remove members" in {
-    val client = TestActorRef(new MockClient(room))
-    client ! Join("Nathan")
-    assert(room.stateData.members.size === 1)
-    info("Successfully joined the room.")
+  "A game room" should {
 
-    client.stop()
-    assert(room.stateData.members.size === 0)
-    info("Successfully left the room.")
+    "start properly" in {
+      assert(room.stateData.members.isEmpty)
+      info("Starts with no members.")
+
+      assert(room.stateData.gamestate === MockGame.init)
+      info("Starts with the initial game state.")
+
+      assert(room.stateName === State.Idle)
+      info("Starts in the Idle state.")
+    }
+
+    // TODO Make these shared tests between states
+
+    "add and remove members" in {
+      val client = TestActorRef(new MockClient(room))
+      client ! Join("Nathan")
+      assert(room.stateData.members.size === 1)
+      info("Successfully joined the room.")
+
+      client.stop()
+      assert(room.stateData.members.size === 0)
+      info("Successfully left the room.")
+    }
+
+    "allow members to change their name" in {
+      val client = TestActorRef(new MockClient(room))
+      client ! Join("Nathan")
+      client ! ChangeName("Sal")
+      assert(room.stateData.members(client).name === "Sal")
+    }
+
+    "respond to update requests" is (pending)
   }
 
-  it should "" is (pending)
+  "A game room (when idle)" should {
+  
+    "add the first members as players" is (pending)
+
+    "allow members to freely change player roles" is (pending)
+
+    "allow members to chat" is (pending)
+
+    "not start the game when there are not enough players" is (pending)
+
+    "start the game when the command is called and there are enough players" is (pending)
+
+    "not allow any other commands" is (pending)
+
+  }
+
+  "A game room (when playing)" should {
+
+    "let a player make valid move" is (pending)
+
+    "not allow a spectator to make a move" is (pending)
+
+    "not allow a player to make an invalid move" is (pending)
+
+    "pause the game if a player leaves" is (pending)
+  }
+
+  "A game room (when paused)" should {
+
+    "resume when the start command is set" is (pending)
+
+    "not allow any moves" is (pending)
+
+  }
+
 }
